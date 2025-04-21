cd7596322e65ecd1e508ae8e745da937cb378213
EvanB6 <evanbartley15@gmail.com>
2025-04-21 19:54:20 -0400

    started Main outline

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter your master password: ");
+            String inputPass = input.nextLine();
+            if (inputPass.equals("Pass")) {
+                break;
+            }
+        }
+            System.out.println("Enter 1 for Create Account, 2 for search for account, 3 for account info.");
+            int userInput = input.nextInt();
     }
 }
